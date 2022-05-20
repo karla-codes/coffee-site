4dd3a80d1b564138afaabecb7d3de64a1427c6fe
karla-walker <indiablonix@gmail.com>
2022-05-20 04:45:06 -0700

    Dropdwon selection and next button fuctionality works

--- src/Components/Options/index.js
+++ src/Components/Options/index.js
@@ -1,29 +1,58 @@
 import React, { useState } from "react"
 import coffeeList from "../../coffee-list"
-import Card from "../Card"
+import RecipeCard from "../RecipeCard"
 
 // needs state for the select element
 // when a drink is selected, display the corresponding recipe card
 
 const Options = () => {
-  const [coffee, setCoffee] = useState("Espresso")
+  const [coffee, setCoffee] = useState(coffeeList[0].name)
+  const [nextCoffee, setNextCoffee] = useState(coffeeList[1].name)
+  const [prevCoffee, setPrevCoffee] = useState(coffeeList[coffeeList.length - 1].name)
 
-  const coffeeSelected = e => {
-    const coffee = e.target.value
-    setCoffee(coffee)
+  const updateCoffeeSelection = e => {
+    const selectedCoffee = e.target.value
+    console.log(selectedCoffee)
+    setCoffee(selectedCoffee)
+    updateNextPrevCoffee(selectedCoffee)
+  }
+
+  const updateNextPrevCoffee = currentCoffee => {
+    // check what coffee is selected
+    // get index for selected coffee
+    // begin scroll at that index
+    // if 'right' btn selected, add 1 to index and setCoffee
+    // if 'left' btn selected, subtract 1 from index and setCoffee
+    // let next
+    // let prev
+    coffeeList.forEach((drink, index) => {
+      if (drink.name === currentCoffee) {
+        const next =
+          index === coffeeList.length - 1 ? coffeeList[0].name : coffeeList[index + 1].name
+        const prev =
+          index === 0 ? coffeeList[coffeeList.length - 1].name : coffeeList[index - 1].name
+        setNextCoffee(next)
+        setPrevCoffee(prev)
+      }
+    })
+  }
+
+  const next = () => {
+    console.log(nextCoffee)
+    setCoffee(nextCoffee)
+    updateNextPrevCoffee(nextCoffee)
   }
 
   return (
     <>
       <div className="intro">
         <p>
-          Scroll through a selection of 12 coffee drink recipes or select a coffee drink from the
+          Scroll through a selection of 18 coffee drink recipes or select a coffee drink from the
           menu below.
         </p>
         <div className="options">
           <label>Select a coffee:</label>
-          <select name="coffee-drinks" id="coffee-selector" onChange={coffeeSelected}>
-            {/* <option>Select a drink</option> */}
+          <select name="coffee-drinks" id="coffee-selector" onChange={updateCoffeeSelection}>
             {coffeeList.map((coffee, index) => (
               <option value={coffee.name} key={index}>
                 {coffee.name}
@@ -32,7 +61,15 @@ const Options = () => {
           </select>
         </div>
       </div>
-      <Card drink={coffee} />
+      <RecipeCard drink={coffee} />
+      <div className="scroll-buttons">
+        <button id="left" onClick={updateNextPrevCoffee}>
+          Left
+        </button>
+        <button id="right" onClick={next}>
+          Right
+        </button>
+      </div>
     </>
   )
 }
