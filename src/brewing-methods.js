49bf023b8a7f783bc1e8242ea4fa697b47216a86
karla-walker <indiablonix@gmail.com>
2022-05-20 04:32:35 -0700

    ✨

---
+++ src/brewing-methods.js
@@ -0,0 +1,29 @@
+const brewingMethods = [
+  {
+    name: "Drip Brew",
+    description:
+      "Ground coffee is added to a brew basket and placed in an automatic coffee machine for this brewing style. Gravity is used to pass water through the grounds, resulting in a traditional cup of coffee.",
+  },
+  {
+    name: "Pour Over",
+    description:
+      "This brewing style is achieved by pouring boiling water slowly through coffee grounds as they sit in a filter basket. The coffee then drips into a single cup, resulting in a potent brew.",
+  },
+  {
+    name: "Cold Brew",
+    description:
+      "For cold brew, coarsely ground coffee is placed in room temperature water and allowed to steep for an extended period of time. This results in a less bitter, highly caffeinated brew.",
+  },
+  {
+    name: "Espresso",
+    description:
+      "To achieve an espresso brew, you'll need an espresso or cappuccino machine. These machines pass pressurized hot water through a filter containing dark roasted finely ground coffee beans. The force of the water produces a highly concentrated coffee shot. This is the method most commonly used for the base of coffee drinks.",
+  },
+  {
+    name: "Ristretto",
+    description:
+      "Brewed in a similar method to the espresso, pressurized water is passed through the coffee grounds. However, you would use half the amount of water. The shorter brewing cycle creates a more concentrated and darker shot of espresso.",
+  },
+]
+
+export default brewingMethods
