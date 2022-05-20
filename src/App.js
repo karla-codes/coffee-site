7c326b9961f677e3cc08940b1d7c89984a75e3f1
karla-walker <indiablonix@gmail.com>
2022-05-20 03:57:03 -0700

    Installed react-router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from "react"
+import { Routes, Route } from "react-router-dom"
 import Header from "./Components/Header"
-import Options from "./Components/Options"
+import Drinks from "./Components/Drinks"
+import BrewingMethods from "./Components/BrewingMethods"
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Options />
+        <Routes>
+          <Route path="/" element={<Drinks />} />
+          <Route path="/brewing-methods" element={<BrewingMethods />} />
+        </Routes>
       </main>
     </>
   )
