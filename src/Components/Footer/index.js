13e1eddb49f7ebbecf76c55ce5ef8fc35c5e579d
karla-walker <indiablonix@gmail.com>
2022-06-01 03:49:23 -0700

    Add a footer to hold references

---
+++ src/Components/Footer/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer>
+      <h2>References</h2>
+      <p>
+        This site uses information from the article{" "}
+        <cite>
+          <a href="https://www.webstaurantstore.com/article/397/types-of-coffee-drinks.html">
+            Types of Coffee Drinks
+          </a>
+        </cite>{" "}
+        by <a href="https://www.webstaurantstore.com/">WebstaurantStore.com</a>
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
