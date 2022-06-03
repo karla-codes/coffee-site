88f67c9b3bc7bbf64462e3c6e626851bdd69cada
karla-walker <indiablonix@gmail.com>
2022-06-03 04:52:16 -0700

    Add function that updates coffee colors based on selected coffee

--- src/Components/RecipeCard/index.js
+++ src/Components/RecipeCard/index.js
@@ -4,41 +4,25 @@ import coffeeList from "../../coffee-list"
 const RecipeCard = props => {
   const selectedCoffee = coffeeList.find(coffee => coffee.name === props.drink)
 
+  const updateCoffeeImage = () => {
+    if (selectedCoffee) {
+      return <div className="coffee-img" style={{ background: `${selectedCoffee.drinkColors}` }} />
+    }
+  }
+
   const displayCoffee = () => {
     if (selectedCoffee) {
       return (
         <>
           <div className="card-wrapper">
-            <div className="card-image">
-              <svg
-                width="306"
-                height="306"
-                viewBox="0 0 306 306"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  x="4.5"
-                  y="4.5"
-                  width="297"
-                  height="297"
-                  fill="#D9D9D9"
-                  stroke="black"
-                  strokeWidth="3"
-                />
-                <rect
-                  x="1.5"
-                  y="1.5"
-                  width="303"
-                  height="303"
-                  fill="#D9D9D9"
-                  stroke="black"
-                  strokeWidth="3"
-                />
-                <rect x="3" y="3" width="300" height="300" fill="#F7F7F7" className="foam" />
-                <rect x="3" y="79" width="300" height="224" fill="#8E8E8E" className="milk" />
-                <rect x="3" y="176" width="300" height="127" fill="#626262" className="espresso" />
-              </svg>
+            {updateCoffeeImage()}
+            <div className="scroll-buttons">
+              <button id="left" aria-label="previous" onClick={props.previousCard}>
+                &larr;
+              </button>
+              <button id="right" aria-label="next" onClick={props.nextCard}>
+                &rarr;
+              </button>
             </div>
             <div className="card-info">
               <h1>{selectedCoffee.name}</h1>
@@ -51,14 +35,6 @@ const RecipeCard = props => {
                 <h2>Recommended cup:</h2>
                 <p>{selectedCoffee.cupType}</p>
               </div>
-            </div>
-            <div className="scroll-buttons">
-              <button id="left" onClick={props.previousCard}>
-                Left
-              </button>
-              <button id="right" onClick={props.nextCard}>
-                Right
-              </button>
             </div>
           </div>
         </>
