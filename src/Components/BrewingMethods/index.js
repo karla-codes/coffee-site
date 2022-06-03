b2d557a107cdb70132a1a5ad5f90c43de04668f5
karla-walker <indiablonix@gmail.com>
2022-06-03 04:30:21 -0700

    Add intro wrapper

--- src/Components/BrewingMethods/index.js
+++ src/Components/BrewingMethods/index.js
@@ -4,10 +4,12 @@ import BrewInfoCard from "../BrewInfoCard"
 const BrewingMethods = () => {
   return (
     <div className="brewing-methods">
-      <p>
-        Not all coffee is brewed in the same way. Different brewing styles can cause changes in the
-        flavor and strength of the drink. Check out a few brewing styles below.
-      </p>
+      <div className="intro">
+        <p>
+          Not all coffee is brewed in the same way. Different brewing styles can cause changes in
+          the flavor and strength of the drink. Check out a few brewing styles below.
+        </p>
+      </div>
       <BrewInfoCard />
     </div>
   )
