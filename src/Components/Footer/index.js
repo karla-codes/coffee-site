33c209bef5ea7a61c0a399f5b8bee5de83595715
karla-walker <indiablonix@gmail.com>
2022-06-05 22:44:57 -0700

    Add copyright to footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,16 +3,23 @@ import React from "react"
 const Footer = () => {
   return (
     <footer>
-      <h2>References</h2>
-      <p>
-        This site uses information from the article{" "}
-        <cite>
-          <a href="https://www.webstaurantstore.com/article/397/types-of-coffee-drinks.html">
-            Types of Coffee Drinks
-          </a>
-        </cite>{" "}
-        by <a href="https://www.webstaurantstore.com/">WebstaurantStore.com</a>
-      </p>
+      <div>
+        <h2>References</h2>
+        <p>
+          This site uses information from the article{" "}
+          <cite>
+            <a href="https://www.webstaurantstore.com/article/397/types-of-coffee-drinks.html">
+              Types of Coffee Drinks
+            </a>
+          </cite>{" "}
+          by <a href="https://www.webstaurantstore.com/">WebstaurantStore.com</a>
+        </p>
+      </div>
+      <div>
+        <p>
+          Copyright 2022 - <a href="https://github.com/karla-codes">Karla Walker</a>
+        </p>
+      </div>
     </footer>
   )
 }
